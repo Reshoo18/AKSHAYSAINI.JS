577b6c62e1f74f0816d9d82e69ff6540a4cffbaf
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-02-26 23:31:02 +0530

    adding something

---
+++ SHORTESTJSCODE/FUNCTIONS/firstclassfun.js
@@ -0,0 +1,15 @@
+                                      //functional statement
+a();
+b();
+                                      
+function a (){
+    console.log("a called")
+}
+
+
+
+
+                                      //functional expression
+ var b=function(){
+    console.log("b called")
+ }
