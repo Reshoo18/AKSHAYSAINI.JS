7a4c427f198145d3b6c7572ebe3c9d986860ca9a
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-02-28 20:39:56 +0530

    first class function

---
+++ SHORTESTJSCODE/FUNCTIONS/Firstclass.js
@@ -0,0 +1,17 @@
+function b(param){
+    console.log(param)
+}
+function xyz(){
+     console.log("called")
+}
+b(xyz);
+
+
+function addNumber(a,b){
+    console.log(a)
+    console.log(b)
+
+}
+const num1 =10;
+const num2 =20;
+addNumber(num1,num2)
