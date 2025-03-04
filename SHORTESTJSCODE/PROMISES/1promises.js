2f04ae495670f65b6c6970e3b9adad5b3e39bc2a
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-03-04 23:17:35 +0530

    promise code

--- SHORTESTJSCODE/PROMISES/1promises.js
+++ SHORTESTJSCODE/PROMISES/1promises.js
@@ -1,3 +1,23 @@
-   let promise = new Promise((resolve,reject)=> {
-    console.log("i am a promise")
-   })
+//PROISE SYNTAX
+
+   // let promise = new Promise((resolve,reject)=> {
+   //  console.log("i am a promise")
+   // })
+
+
+
+         // ASYNCFUNCTION PROMICES
+
+function asyncFunc() {
+   return new Promise((resolve, reject) => {
+   setTimeout(()=>{
+      console.log("data1");
+      resolve("sucess");
+   }, 4000);
+});
+}
+console.log("fetching data 1");
+let p1=asyncFunc();
+p1.then((res)=>{
+console.log(res);
+});
