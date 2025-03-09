05bbace861d6c90d21d3877a2cf6fd4bc7d3eef0
Rajnish Ranjan <ranjanrajnish64@gmail.com>
2025-03-09 13:17:33 +0530

    project code

--- SHORTESTJSCODE/PROMISES/1promises.js
+++ SHORTESTJSCODE/PROMISES/1promises.js
@@ -8,16 +8,44 @@
 
          // ASYNCFUNCTION PROMICES
 
-function asyncFunc() {
-   return new Promise((resolve, reject) => {
-   setTimeout(()=>{
-      console.log("data1");
-      resolve("sucess");
-   }, 4000);
-});
+// function asyncFunc() {
+//    return new Promise((resolve, reject) => {
+//    setTimeout(()=>{
+//       console.log("data1");
+//       resolve("sucess");
+//    }, 4000);
+// });
+// }
+// console.log("fetching data 1");
+// let p1=asyncFunc();
+// p1.then((res)=>{
+// console.log(res);
+// });
+
+
+
+function asynFunc1(){
+   return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+         console.log("some data1")
+         resolve("success")
+      },5000)
+   });
 }
-console.log("fetching data 1");
-let p1=asyncFunc();
+function asynFunc2(){
+   return new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+         console.log("some data2")
+         resolve("success")
+      },5000)
+   });
+}
+
+console.log("fetching data1")
+let p1=asynFunc1();
 p1.then((res)=>{
-console.log(res);
+  console.log("fetching data2");
+  let p2=asynFunc2();
+  p2.then((res)=>{});
+   
 });
